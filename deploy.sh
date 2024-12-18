ba29fad26593459c638ec9225ba97296bcf32705
zhangtq <15652533044@163.com>
2024-12-18 12:47:30 +0800

    '修改deploy脚本'

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 hexo clean
 hexo g
 hexo d
+# 通过中转服务器，将代码同步更新到github
+ssh zhangtianqing@www.zhangtq.com -p 22 "cd /home/zhangtianqing/beautify_cnblogs && git pull gitee master && git merge github/master && git push github master"
